df1b332739430526f70d51c7e61de63c741c3903
Marcelo Barbosa <marcelo.castilho2707@gmail.com>
2023-01-14 16:05:11 -0300

    feat: adding header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,37 @@
-import { Text} from 'react-native'
-import { Container, Title } from './styles'
+import { RFValue } from "react-native-responsive-fontsize";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+} from "./styles";
 
 const Dashboard = () => {
-  return(
+  return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/108136296?s=400&v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Marcelo</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power"/>
+        </UserWrapper>
+      </Header>
     </Container>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
