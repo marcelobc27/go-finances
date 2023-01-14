385c41077fec577a6ef10569ed4ad0fe53ad0191
Marcelo Barbosa <marcelo.castilho2707@gmail.com>
2023-01-14 11:24:58 -0300

    feat: adding missing dependencies

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Text} from 'react-native'
+import { Container, Title } from './styles'
+
+const Dashboard = () => {
+  return(
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  )
+}
+
+export default Dashboard
